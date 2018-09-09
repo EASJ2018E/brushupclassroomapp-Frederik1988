1b16ae89b26439eb8a610ba9c6a90dd1bcb46376
Frederik Skærbæk Jensen <fred15b4@edu.easj.dk>
2018-09-09 11:13:15 +0200

    Klassen SemesterClassRoom er oprettet. Jeg har rettet alle properties og klasser til engelsk da jeg finder det mest naturligt at kode på engelsk

---
+++ ClassRoom/SemesterClassRoom.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    class SemesterClassRoom
+    {
+        public string ClassName { get; set; }
+        List<Student> classList = new List<Student>();
+        public DateTime SemesterStart { get; set; }
+
+
+        public SemesterClassRoom()
+        {
+           
+        }
+    }
+}
