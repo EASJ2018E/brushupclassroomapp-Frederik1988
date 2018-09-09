e70c20dfa63bfa7f6c7157bb69c027bfc09834e9
Frederik Skærbæk Jensen <fred15b4@edu.easj.dk>
2018-09-09 11:00:16 +0200

    Klassen studerende er oprettet med properties og constructor

---
+++ ClassRoom/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    class Student
+    {
+        private string _name;
+        private int _birthMonth;
+        private int _birthDate;
+
+        public Student(string name, int birthMonth, int birthDate)
+        {
+            name = _name;
+            birthMonth = _birthMonth;
+            birthDate = _birthDate;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public int BirthMonth
+        {
+            get { return _birthMonth; }
+        }
+
+        public int BirthDate
+        {
+            get { return _birthDate; }
+        }
+    }
+
+}
